43b1fffd165156525d247e7da9382f6f4dba59f3
alexandrazykova <alexandrazykova1@gmail.com>
2023-08-22 15:03:21 -0400

    added function print results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var searchFormEl = document.querySelector('#search-form');
-var searchInputVal = document.querySelector('#search-input').value;
+var searchInputVal = document.querySelector ('#search-input').value;
 var APIKey = "2871ad72202319cf561e97ce31c20f63";
 
 var resultTextEl = document.querySelector('#result-text');
@@ -7,9 +7,35 @@ var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form');
 
 
-function printResults(resultObj) {
+
+function printResults (resultObj) {
     console.log(resultObj);
-}
+
+    // set up `<div>` to hold result content
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card');
+
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  var cityName = document.createElement('h3');
+  cityName.textContent = resultObj.name;
+
+  var bodyContentEl = document.createElement('p');
+  bodyContentEl.innerHTML = resultObj.name;
+
+  if (resultObj.name) {
+    bodyContentEl.innerHTML += resultObj.name.join();
+  } else {
+    bodyContentEl.innerHTML += 'No subject for this entry.';
+  }
+
+  resultBody.append(cityName, bodyContentEl);
+
+  resultContentEl.append(resultCard);
+};
+
 
 function searchApi(searchInputVal) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputVal + "&appid=" + APIKey;
