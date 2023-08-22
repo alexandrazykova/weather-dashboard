a96dc7980559db6a59779a753765b9560b3c7166
alexandrazykova <alexandrazykova1@gmail.com>
2023-08-22 09:49:24 -0400

    fixed bug in fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,18 @@
 var searchFormEl = document.querySelector('#search-form');
 var searchInputVal = document.querySelector('#search-input').value;
 var APIKey = "2871ad72202319cf561e97ce31c20f63";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputVal + "&appid=" + APIKey;
+
 var resultTextEl = document.querySelector('#result-text');
 var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form');
 
-// Get the search params out of the URL
-function getParams() {
-    var searchParamsArr = document.location.search.split("&");
-
-    // Get the query and format values   
-    var query = searchParamsArr[0].split("=").pop();
-    var appid = searchParamsArr[1].split("=").pop();
-
-    searchApi(query, appid);
-}
 
 function printResults(resultObj) {
     console.log(resultObj);
 }
 
-function searchApi(query, appid) {
+function searchApi(searchInputVal) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputVal + "&appid=" + APIKey;
 
     fetch(queryURL)
         .then(function (response) {
@@ -32,9 +23,9 @@ function searchApi(query, appid) {
             return response.json();
         })
         .then(function (dataRes) {
-            resultTextEl.textContentEl = dataRes.search.query;
 
-            console.log(locRes);
+            console.log(dataRes);
+
 
             if (!dataRes.results.length) {
                 console.log('No results found!');
@@ -48,24 +39,20 @@ function searchApi(query, appid) {
         })
         .catch(function (error) {
             console.error(error);
-
-        })
+        });
 }
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
 
     var searchInputVal = document.querySelector('#search-input').value;
-    var formatInputVal = document.querySelector('#format-input').value;
 
     if (!searchInputVal) {
         console.error('Please enter you search');
         return;
     }
 
-    searchApi(searchInputVal, formatInputVal);
+    searchApi(searchInputVal);
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-getParams();
