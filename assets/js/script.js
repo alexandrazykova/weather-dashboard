e710d7c4a12596ebb856c535a3f62c4c3a7c6abd
alexandrazykova <alexandrazykova1@gmail.com>
2023-08-22 16:10:50 -0400

    fixed button bug and print result function call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,38 @@
 var searchFormEl = document.querySelector('#search-form');
-var searchInputVal = document.querySelector ('#search-input').value;
+var searchInputVal = document.querySelector('#search-input').value;
 var APIKey = "2871ad72202319cf561e97ce31c20f63";
-
 var resultTextEl = document.querySelector('#result-text');
 var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
+var searchBtn = document.querySelector('#btn');
 
 
 
-function printResults (resultObj) {
+function printResults(resultObj) {
     console.log(resultObj);
 
     // set up `<div>` to hold result content
-  var resultCard = document.createElement('div');
-  resultCard.classList.add('card');
+    var resultCard = document.createElement('div');
+    resultCard.classList.add('card');
 
-  var resultBody = document.createElement('div');
-  resultBody.classList.add('card-body');
-  resultCard.append(resultBody);
+    var resultBody = document.createElement('div');
+    resultBody.classList.add('card-body');
+    resultCard.append(resultBody);
 
-  var cityName = document.createElement('h3');
-  cityName.textContent = resultObj.name;
+    var cityName = document.createElement('h3');
+    cityName.textContent = resultObj.name;
 
-  var bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML = resultObj.name;
+    var bodyContentEl = document.createElement('p');
+    bodyContentEl.innerHTML = resultObj.name;
 
-  if (resultObj.name) {
-    bodyContentEl.innerHTML += resultObj.name.join();
-  } else {
-    bodyContentEl.innerHTML += 'No subject for this entry.';
-  }
+    if (resultObj.name) {
+        bodyContentEl.innerHTML += resultObj.name.join();
+    } else {
+        bodyContentEl.innerHTML += 'No subject for this entry.';
+    }
 
-  resultBody.append(cityName, bodyContentEl);
+    resultBody.append(cityName, bodyContentEl);
 
-  resultContentEl.append(resultCard);
+    resultContentEl.append(resultCard);
 };
 
 
@@ -45,28 +44,24 @@ function searchApi(searchInputVal) {
             if (!response.ok) {
                 throw response.json();
             }
-
             return response.json();
         })
         .then(function (dataRes) {
-
             console.log(dataRes);
 
 
-            if (!dataRes.results.length) {
+            if (!dataRes.name) {
                 console.log('No results found!');
                 resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
             } else {
                 resultContentEl.textContent = '';
-                for (var i = 0; i < dataRes.results.length; i++) {
-                    printResults(dataRes.results[i]);
-                }
+                printResults(dataRes);
             }
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-}
+        }
+            .catch(function (error) {
+                console.error(error);
+            })
+)}
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
@@ -81,4 +76,4 @@ function handleSearchFormSubmit(event) {
     searchApi(searchInputVal);
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+searchBtn.addEventListener('click', handleSearchFormSubmit); 
